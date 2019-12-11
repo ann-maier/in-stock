fa46c97994135a5dad0b061bbd4c0ccc0a5afca6
Anton Trofymchuk <Anton.Trofymchuk@thomsonreuters.com>
2019-12-11 19:57:03 +0200

    add for products and warehouses

--- server/routes/products.js
+++ server/routes/products.js
@@ -15,4 +15,36 @@ router.get("/", (request, response) => {
   });
 });
 
+router.post("/", (request, response) => {
+  const { type, title, price } = request.body;
+
+  fs.readFile("database/products.json", (error, data) => {
+    if (error) {
+      response.status(404).json({
+        error
+      });
+    }
+
+    const parsedData = JSON.parse(data);
+    console.log(parsedData.products);
+    let lastId = parsedData.products.reduce((lastId, { productId }) => {
+      if (lastId < productId) {
+        lastId = productId;
+      }
+      return lastId;
+    }, 0);
+
+    parsedData.products.push({ productId: lastId + 1, type, title, price });
+
+    fs.writeFile("database/products.json", JSON.stringify(parsedData), (error) => {
+      if (error) {
+        response.status(404).json({
+          error
+        });
+      }
+      response.status(200).send();
+    });
+  });
+});
+
 module.exports = router;
